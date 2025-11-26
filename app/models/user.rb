d09d0839ff090151831441bd83200eaed817aa30
Colton Sikes <colton.sikes@gmail.com>
2025-11-26 14:44:20 +0900

    Updated dashboard and models to assist in obtain necessary data

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :partnership
+  has_one :partnership_as_one, class_name: "Partnership", foreign_key: "user_one_id"
+  has_one :partnership_as_two, class_name: "Partnership", foreign_key: "user_two_id"
   has_many :journals
+
+  def partnership
+    partnership_as_one || partnership_as_two
+  end
 end
